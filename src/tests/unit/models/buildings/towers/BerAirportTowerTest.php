ffc767096d7a7f660e19e04da662af69e9b85489
Kevin Frantz <mail@kevin-frantz.de>
2018-06-14 16:15:39 +0200

    Removed unnecessary imports

--- src/tests/unit/models/buildings/towers/BerAirportTowerTest.php
+++ src/tests/unit/models/buildings/towers/BerAirportTowerTest.php
@@ -3,8 +3,7 @@ namespace tests\unit\models\buildings\towers;
 
 use PHPUnit\Framework\TestCase;
 use models\buildings\towers\BerAirportTower;
-use models\virtual\permissions\PermissionToLand;
-use models\vehicles\planes\A380;
+
 
 /**
  *
