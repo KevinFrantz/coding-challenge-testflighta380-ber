d4e73b085d64ea7a005ccc1e6b0439088c404ec8
Kevin Frantz <mail@kevin-frantz.de>
2018-06-15 13:38:24 +0200

    Added some empty tests

---
+++ src/tests/unit/controller/FlightControllerTest.php
@@ -0,0 +1,19 @@
+<?php
+namespace tests\unit\controller;
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ *
+ * @author kevinfrantz
+ *        
+ */
+class FlightControllerTest extends TestCase
+{
+    protected $controller;
+    
+    protected function setUp(){
+        
+    }
+}
+
---
+++ src/tests/unit/controller/MoveControllerTest.php
@@ -0,0 +1,19 @@
+<?php
+namespace tests\unit\controller;
+
+use controller\MoveController;
+
+/**
+ *
+ * @author kevinfrantz
+ *        
+ */
+class MoveControllerTest extends MoveControllerTest
+{
+    protected $controller;
+    
+    protected function setUp(){
+        $this->controller = new MoveController();
+    }
+}
+
