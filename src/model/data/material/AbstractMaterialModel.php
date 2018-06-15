21f48626ec51021a9d0c5ca41e2cbb5327edb578
Kevin Frantz <mail@kevin-frantz.de>
2018-06-15 13:38:49 +0200

    Added AbstractMaterialModel

--- src/model/data/material/AbstractMaterialModel.php
+++ src/model/data/material/AbstractMaterialModel.php
@@ -2,13 +2,14 @@
 namespace model\data\material;
 
 use model\data\position\AbstractPosition;
+use interfaces\model\data\material\MaterialInterface;
 
 /**
  *
  * @author kevinfrantz
  *        
  */
-abstract class AbstractMaterialModel
+abstract class AbstractMaterialModel implements MaterialInterface
 {
     /**
      * @var AbstractPosition
