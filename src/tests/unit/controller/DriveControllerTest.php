7edab63185c487c29529e6930b84bd01f6ac4ea8
Kevin Frantz <mail@kevin-frantz.de>
2018-06-16 15:16:03 +0200

    Optimized controller tests

--- src/tests/unit/controller/DriveControllerTest.php
+++ src/tests/unit/controller/DriveControllerTest.php
@@ -3,10 +3,12 @@ namespace tests\unit\controller;
 
 use PHPUnit\Framework\TestCase;
 use model\method\collection\GuestCollection;
+use model\method\material\building\terminal\BerAirportTerminal as BerAirportTerminalOrigin;
 use model\method\material\building\terminal\BerAirportTerminal;
 use controller\move\DriveController;
 use interfaces\model\method\move\MovingInterface;
 use model\data\position\Geo;
+use model\data\material\building\airport\BER;
 
 /**
  *
@@ -15,22 +17,34 @@ use model\data\position\Geo;
  */
 class DriveControllerTest extends TestCase
 {
-
+    /**
+     * @var DriveController
+     */
     protected $controller;
 
+    /**
+     * @var BerAirportTerminal
+     */
     protected $target;
 
+    /**
+     * @var GuestCollection 
+     */
     protected $movingElement;
 
     protected function setUp(): void
     {
         $this->movingElement = new GuestCollection();
-        $this->movingElement->setMovingPosition(new Geo());
-        $this->target = new BerAirportTerminal();
+        $this->movingElement->setPosition(new Geo());
+        $this->target = new BerAirportTerminal(
+            new BerAirportTerminalOrigin(
+                new BER()
+                )
+            );
         $this->controller = new DriveController($this->movingElement);
     }
     
-    public function testGetter(){
+    public function testGetter():void{
         $this->assertInstanceOf(MovingInterface::class, $this->controller->getMovingElement());
     }
 
--- src/tests/unit/controller/FlightControllerTest.php
+++ src/tests/unit/controller/FlightControllerTest.php
@@ -2,9 +2,12 @@
 namespace tests\unit\controller;
 
 use model\method\material\vehicle\plane\A380;
-use model\method\material\building\runway\BerAirportRunway2;
+use model\data\material\vehicle\plane\A380 as A380Original;
+use model\method\material\building\runway\AirportRunway;
 use controller\move\FlightController;
 use model\data\position\Sky;
+use model\data\material\building\airport\BER;
+use model\data\material\building\runway\BerAirportRunway2;
 
 /**
  *
@@ -16,9 +19,9 @@ class FlightControllerTest extends DriveControllerTest
 
     protected function setUp(): void
     {
-        $this->movingElement = new A380();
-        $this->movingElement->setMovingPosition(new Sky());
-        $this->target = new BerAirportRunway2();
+        $this->movingElement = new A380(new A380Original());
+        $this->movingElement->setPosition(new Sky());
+        $this->target = new AirportRunway(new BerAirportRunway2(new BER()));
         $this->controller = new FlightController($this->movingElement);
     }
 }
