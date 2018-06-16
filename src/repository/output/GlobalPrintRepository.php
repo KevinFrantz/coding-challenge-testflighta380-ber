7608ed093eee150a43fac51cbf3389a1530e0073
Kevin Frantz <mail@kevin-frantz.de>
2018-06-16 18:55:13 +0200

    Optimized Implementation GlobalPrintRepository

--- src/repository/output/GlobalPrintRepository.php
+++ src/repository/output/GlobalPrintRepository.php
@@ -1,13 +1,15 @@
 <?php
 namespace repository\output;
 
+use interfaces\repository\output\PrintRepositoryInterface;
+
 /**
  * This Repository containes one instanz of the print Repository.
  * It allows global output e.g. for debugging
  * @author kevinfrantz
  *        
  */
-class GlobalPrintRepository extends PrintRepository
+class GlobalPrintRepository implements PrintRepositoryInterface
 {
     /**
      * @var $this
@@ -17,8 +19,24 @@ class GlobalPrintRepository extends PrintRepository
     public function __construct(){
         parent::__construct();
         if(!self::$instance){
-            self::$instance = $this;
+            self::$instance = new PrintRepository();
         }
     }
+    
+    public function printOutput(): void
+    {
+        self::$instance->printOutput();
+    }
+    public function addOutput($output): void
+    {
+        self::$instance->addOutput($output);
+    }
+
+    public function getOutput()
+    {
+        return self::$instance->getOutput();
+    }
+
+
 }
 
