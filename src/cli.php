cd9cf22079718c28df7eed62270dbc1bfb3d8861
Kevin Frantz <mail@kevin-frantz.de>
2018-06-16 09:29:29 +0200

    Added memory_limit to prevent crashs

--- src/cli.php
+++ src/cli.php
@@ -2,7 +2,7 @@
     use scenario\TestflightA380Ber\TestflightA380Ber;
 
     require __DIR__. '/vendor/autoload.php';
-    print(shell_exec('php ./vendor/bin/phpunit'));
+    print(shell_exec('php -d memory_limit=128M vendor/bin/phpunit'));
     $standartScenario = new TestflightA380Ber();
     $standartScenario->play();
 ?>
