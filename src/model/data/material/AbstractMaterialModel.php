c28fcce5256f067fafb0bb826cfab89342786be1
Kevin Frantz <mail@kevin-frantz.de>
2018-06-17 13:25:13 +0200

    Added constructor

--- src/model/data/material/AbstractMaterialModel.php
+++ src/model/data/material/AbstractMaterialModel.php
@@ -4,6 +4,7 @@ namespace model\data\material;
 use model\data\position\AbstractPosition;
 use interfaces\model\data\material\MaterialInterface;
 use interfaces\model\data\position\PositionInterface;
+use model\data\position\Sky;
 
 /**
  *
@@ -22,6 +23,11 @@ abstract class AbstractMaterialModel implements MaterialInterface
      * @var string
      */
     protected $name;
+    
+    public function __construct(){
+        $this->name = 'noname';
+        $this->position = new Sky();
+    }
     
     /**
      * {@inheritDoc}
